e65276c06d8d836f6d84899757ce058c56648a99
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-05-26 01:52:25 +0200

    Update English localization strings

--- src/webparts/spTeamFooter/loc/en-us.js
+++ src/webparts/spTeamFooter/loc/en-us.js
@@ -1,7 +1,7 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
+    "PropertyPaneDescription": "Configure the Team Footer web part",
+    "BasicGroupName": "Settings",
     "DescriptionFieldLabel": "Description Field",
     "AppLocalEnvironmentSharePoint": "The app is running on your local environment as SharePoint web part",
     "AppLocalEnvironmentTeams": "The app is running on your local environment as Microsoft Teams app",
@@ -11,6 +11,10 @@ define([], function() {
     "AppTeamsTabEnvironment": "The app is running in Microsoft Teams",
     "AppOfficeEnvironment": "The app is running in office.com",
     "AppOutlookEnvironment": "The app is running in Outlook",
-    "UnknownEnvironment": "The app is running in an unknown environment"
+    "UnknownEnvironment": "The app is running in an unknown environment",
+    "ListFieldLabel": "Select List",
+    "CenterDirectorFieldLabel": "Center Director",
+    "CreateNewListLabel": "Create New List",
+    "ViewSelectedListLabel": "View Selected List"
   }
 });
