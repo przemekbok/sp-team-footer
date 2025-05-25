1b6d9a6f2bf937cfad58417aca8e1b1c15586ca1
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-05-26 01:46:06 +0200

    Update component props interface with required data types

--- src/webparts/spTeamFooter/components/ISpTeamFooterProps.ts
+++ src/webparts/spTeamFooter/components/ISpTeamFooterProps.ts
@@ -1,7 +1,35 @@
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+import { SPHttpClient } from '@microsoft/sp-http';
+
 export interface ISpTeamFooterProps {
-  description: string;
+  listId: string;
+  centerDirector: string;
+  centerDirectorData: any;
   isDarkTheme: boolean;
   environmentMessage: string;
   hasTeamsContext: boolean;
   userDisplayName: string;
+  context: WebPartContext;
+  httpClient: SPHttpClient;
+  siteUrl: string;
+}
+
+export interface ICenterManager {
+  id: number;
+  title: string;
+  email: string;
+  picture?: string;
+  department?: string;
+  jobTitle?: string;
+  location?: string;
+}
+
+export interface ITeamData {
+  id: number;
+  teamName: string;
+  teamDescription: string;
+  locations: string[];
+  teamLeaders: any[];
+  techLeaders: any[];
+  centerManager: any;
 }
